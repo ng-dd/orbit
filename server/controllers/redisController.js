9e21396e4ead55803a182351d588c4baeefbd13c
Daniel Kim <dkim099@gmail.com>
2017-10-19 14:58:30 -0700

    services

--- server/controllers/redisController.js
+++ server/controllers/redisController.js
@@ -4,16 +4,29 @@ module.exports = {
     getSomething: function(req, res) {
         //req.params.id
         redis.redisClient.get(req.query.key, (err, reply) => {
-           if (reply !== null) {
-               res.send(reply);
-           } else {
-               
-           }
+            if (err) {
+                return console.log(err);
+            }
+
+            if (reply !== null) {
+                res.send(reply);
+            } else {
+                //query postgres to add to cache
+                //then set it to expire
+            }
         })
+    },
+    
+    getLatestCache: function(req, res) {
+        let now = Date.now();
+        let minuteAgo = now - 60000; 
     },
 
     postSomething: function(req, res) {
         redis.redisClient.set(req.body.key, req.body.value)
+        
+        //wipe cache info from redis after 10 minutes
+        redis.redisClient.expire(req.body.key, 600)
         res.status(200)
         res.end()
     }
